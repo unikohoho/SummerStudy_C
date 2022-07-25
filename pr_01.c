440829d2526a4f84d6ac60fba0ed65dc00c0bd08
USER <USER@A_NOTE>
2022-07-26 03:28:13 +0900

    초기 커밋.

---
+++ pr_01.c
@@ -0,0 +1,17 @@
+//한 줄 단위 주석문
+
+/*여러줄 
+주석문*/
+
+//Practice_01) 간단한 화면 출력 연습
+
+#include<stdio.h>
+
+int main(void) {
+
+	printf("~C언어 연습을 시작합니다~\n");
+	printf("이름: 고운이");
+
+	return 0;
+}
+//실행은 컨트롤 + f5
